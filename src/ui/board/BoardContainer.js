69ae4399e450021bb704d1547817f0d06c9c0714
Brian David <beejunk@gmail.com>
2020-09-19 22:31:51 -0500

    refactor(BoardContainer): Switch to function with hooks

--- src/ui/board/BoardContainer.js
+++ src/ui/board/BoardContainer.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent as Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Board from "./Board";
 import type {
   GameChannel,
@@ -11,7 +11,7 @@ import type {
 type Props = {
   game: GameChannel,
   playing?: boolean,
-  onClickPoint: (
+  onClickPoint?: (
     game: GameChannel,
     loc: Point,
     color?: ?PlayerColor,
@@ -24,88 +24,80 @@ type State = {
   marginTop: number,
 };
 
-export default class BoardContainer extends Component<Props, State> {
-  static defaultProps: Props;
-  state: State = { boardWidth: null, marginTop: 0 };
+export default function BoardContainer(props: Props) {
+  let { game, onClickPoint } = props;
+  let { tree } = game;
+  let board;
+  let markup;
 
-  _containerRef: ?HTMLElement;
+  let [state, setState] = useState<State>({ boardWidth: null, marginTop: 0 });
+  let { boardWidth, marginTop } = state;
 
-  _setBoardWidth = () => {
-    if (this._containerRef) {
+  let containerRef = useRef(null);
+
+  function setBoardWidth() {
+    if (containerRef.current) {
       // Note: this is tightly coupled to the CSS layout
-      let containerWidth = this._containerRef.offsetWidth;
-      let containerHeight = this._containerRef.offsetHeight;
-      let boardWidth = Math.min(containerWidth, containerHeight);
-      let marginTop = -35;
-      if (containerWidth <= 736 || containerWidth - boardWidth < 180) {
-        boardWidth = Math.min(containerWidth, containerHeight - 35);
-        marginTop = 0;
+      let containerEl = containerRef.current;
+      let containerWidth = containerEl.offsetWidth;
+      let containerHeight = containerEl.offsetHeight;
+      let nextBoardWidth = Math.min(containerWidth, containerHeight);
+      let nextMarginTop = -35;
+      if (containerWidth <= 736 || containerWidth - nextBoardWidth < 180) {
+        nextBoardWidth = Math.min(containerWidth, containerHeight - 35);
+        nextMarginTop = 0;
       }
-      this.setState({ boardWidth, marginTop });
+      setState({ boardWidth: nextBoardWidth, marginTop: nextMarginTop });
     }
-  };
-
-  componentDidMount() {
-    this._setBoardWidth();
-    window.addEventListener("resize", this._setBoardWidth);
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this._setBoardWidth);
-  }
-
-  render() {
-    let { game, onClickPoint } = this.props;
-    let { boardWidth, marginTop } = this.state;
-
-    if (!boardWidth) {
-      return (
-        <div
-          className="GameScreen-board-container"
-          ref={this._setContainerRef}
-        />
-      );
-    }
-
-    let tree = game.tree;
-    let board;
-    let markup;
-    if (tree) {
-      let computedState = tree.computedState[tree.currentNode];
-      if (computedState) {
-        board = computedState.board;
-        markup = computedState.markup;
-      }
-    }
-    return (
-      <div className="GameScreen-board-container" ref={this._setContainerRef}>
-        <div
-          className="GameScreen-board"
-          style={{ width: boardWidth, height: boardWidth, marginTop }}>
-          <div className="GameScreen-board-inner">
-            {board && markup ? (
-              <Board
-                board={board}
-                markup={markup}
-                width={boardWidth}
-                onClickPoint={onClickPoint ? this._onClickPoint : undefined}
-              />
-            ) : null}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  _setContainerRef = (ref: HTMLElement | null) => {
-    this._containerRef = ref;
-  };
-
-  _onClickPoint = (
+  function onClickPointHandler(
     loc: Point,
     color?: ?PlayerColor,
     mark?: ?BoardPointMark
-  ) => {
-    this.props.onClickPoint(this.props.game, loc, color, mark);
-  };
+  ) {
+    if (onClickPoint) {
+      onClickPoint(props.game, loc, color, mark);
+    }
+  }
+
+  useEffect(() => {
+    setBoardWidth();
+    window.addEventListener("resize", setBoardWidth);
+
+    return () => {
+      window.removeEventListener("resize", setBoardWidth);
+    };
+  }, []);
+
+  if (!boardWidth) {
+    return <div className="GameScreen-board-container" ref={containerRef} />;
+  }
+
+  if (tree) {
+    let computedState = tree.computedState[tree.currentNode];
+    if (computedState) {
+      board = computedState.board;
+      markup = computedState.markup;
+    }
+  }
+
+  return (
+    <div className="GameScreen-board-container" ref={containerRef}>
+      <div
+        className="GameScreen-board"
+        style={{ width: boardWidth, height: boardWidth, marginTop }}>
+        <div className="GameScreen-board-inner">
+          {board && markup ? (
+            <Board
+              board={board}
+              markup={markup}
+              width={boardWidth}
+              onClickPoint={onClickPoint ? onClickPointHandler : undefined}
+            />
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
